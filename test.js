4f3dd15311c49e4c64a0c37b7c4f315a8ce8c16b
Rheno001 <renfredreuben@gmail.com>
2022-12-18 16:30:49 +0100

    no change

---
+++ test.js
@@ -0,0 +1,7 @@
+//navbar
+function toggleMenu (){
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.dropdown-menu');
+    menuToggle.classList.toggle('active')
+    menu.classList.toggle('active')
+}
